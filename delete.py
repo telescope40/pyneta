423a229fe3292a746c5b5158cd9de72b59e74d4b
telescope <telescope40@gmail.com>
2019-02-09 14:35:46 -0800

    Week 2 Part 2

---
+++ delete.py
@@ -0,0 +1,19 @@
+import netmiko
+import getpass
+
+cisco3 = {
+  'host':'cisco3.lasthop.io',
+  'username': 'pyclass',
+  'password':'88newclass',
+  'device_type':'cisco_ios',
+}
+
+net_connect = netmiko.ConnectHandler(**cisco3)
+print(net_connect.find_prompt())
+
+command = 'delete mytest'
+net_connect.send_command(command, expect_string=r'?',
+                            strip_prompt=False, strip_command=False)
+output = net_connect.send_command('y', expect_string=r'#',
+                            strip_prompt=False, strip_command=False)
+print(output)
